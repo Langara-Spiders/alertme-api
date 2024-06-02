aa8e6843496b1d0efc37843d355559d34528bba3
BryanCaldeira <bryancaldeira22@gmail.com>
2024-06-02 01:59:10 -0700

    chore: add redirect to docs

--- app/app/settings.py
+++ app/app/settings.py
@@ -32,16 +32,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
