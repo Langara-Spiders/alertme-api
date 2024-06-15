1ec30bebaa696d2f9ac3bf21f6dbf3c83041ebe1
Sandeep Guggilla <sandeepguggilla@Sandeeps-MacBook-Air.local>
2024-06-15 02:15:51 -0700

    feat: 106 | incident and notification modals added

--- app/core/models.py
+++ app/core/models.py
@@ -10,6 +10,29 @@ from django.contrib.auth.models import (
 )
 
 
+
+# The `INCIDENT_STATUS_CHOICES` constant is defining a list of choices for the status of an incident.
+# Each choice is a tuple where the first element is the value stored in the database and the second
+# element is a human-readable representation of that value.
+INCIDENT_STATUS_CHOICES = [
+    ('NONE', 'None'),
+    ('ACTIVE', 'Active'),
+    ('PENDING', 'Pending'),
+    ('FIXING', 'Fixing'),
+    ('RESOLVED', 'Resolved'),
+]
+
+# The `REPORTED_BY_CHOICES` constant is defining a list of choices for the reported by field in the
+# database models. It provides a set of options for who reported an incident, with each choice
+# represented as a tuple. The first element of the tuple is the value that will be stored in the
+# database, and the second element is a human-readable representation of that value.
+REPORTED_BY_CHOICES = [
+    ('USER', 'User'),
+    ('ORG', 'Org'),
+]
+
+
+
 class Organization(models.Model):
     _id = models.UUIDField(
         primary_key=True,
@@ -106,3 +129,74 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
+    
+    
+    
+    
+class IncidentCategory(models.Model):
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=25)
+    icon = models.FileField(upload_to='incident_category_icons/')
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        db_table = 'incident_categories'
+        verbose_name = "Incident Category"
+        verbose_name_plural = "Incident Categories"
+        
+        
+class IncidentImage(models.Model):
+    image = models.FileField(upload_to='incident_images')
+    class Meta:
+        db_table = 'incident_images'
+        
+        
+class Incident(models.Model):
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    images = models.ForeignKey(IncidentImage, on_delete=models.PROTECT, null=True, blank=True)
+    incident_category_id = models.ForeignKey(IncidentCategory, on_delete=models.PROTECT)
+    subject = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    coordinate = models.JSONField(default=dict)
+    upvote_count = models.IntegerField(default=0)
+    report_count = models.IntegerField(default=0)
+    status = models.CharField(
+        max_length=10,
+        choices=INCIDENT_STATUS_CHOICES,
+        default=INCIDENT_STATUS_CHOICES[0][0]
+    )
+    is_accepted_by_org = models.BooleanField(default=False)
+    is_internal_for_org = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    reported_by = models.CharField(
+        max_length=4,
+        choices=REPORTED_BY_CHOICES,
+        default=REPORTED_BY_CHOICES[0][0]
+    )
+    voters = models.ManyToManyField(User, related_name='voted_incidents', blank=True, )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['created_at']
+        db_table = 'incidents'
+    
+    
+class NotificationList(models.Model):
+    _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    incident_id = models.ForeignKey(Incident, on_delete=models.PROTECT)
+    subject = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+        db_table = 'notifications_list'
+        verbose_name = "Notification List"
+        verbose_name_plural = "Notifications List"
+
