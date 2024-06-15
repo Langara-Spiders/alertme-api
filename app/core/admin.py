d065617711a1dc2098b33a18db18d3ba47b505f3
BryanCaldeira <bryancaldeira22@gmail.com>
2024-06-14 21:48:44 -0700

    feat: create user model, add S3 storage support

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,13 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from .models import (
+    User,
+    Organization,
+    Project,
+)
+
+
+admin.register(
+    User,
+    Organization,
+    Project,
+)(admin.ModelAdmin)
