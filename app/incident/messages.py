d2e20ba825c4cde29c73fe90cb532d302cd2788f
Sandeep Guggilla <sandeepguggilla@Sandeeps-MacBook-Air.local>
2024-06-17 19:33:15 -0700

    feat: 104 | Added messages and proper status messages

---
+++ app/incident/messages.py
@@ -0,0 +1,13 @@
+MESSAGES = {
+    'en-CA': {
+        'ERROR_MESSAGE_INVALID_TOKEN': 'An error occurred: {}',
+        'ERROR_MESSAGE_INVALID_DATA': 'Invalid data provided: {}',
+        'ERROR_MESSAGE_NOT_FOUND': 'Resource not found: {}',
+        'ERROR_MESSAGE_MISSING_ID': 'Incident ID is required.',
+        'SUCCESS_MESSAGE': 'Incident retrieved successfully',
+        'SUCCESS_VOTE_MESSAGE': 'Your vote has been recorded.',
+        'ALREADY_VOTED_MESSAGE': 'You have already voted on this incident.',
+        'SUCCESS_REPORT_MESSAGE': 'Incident reported successfully'
+    },
+    # Add more languages as needed
+}
--- app/incident/views.py
+++ app/incident/views.py
@@ -13,31 +13,48 @@ from core.utils import upload_file_to_s3
 from core.models import IncidentImage, Incident
 from core.models import IncidentCategory
 from django.shortcuts import get_object_or_404
+from http import HTTPStatus
+from .messages import MESSAGES
 
 
 # API Logic for Incident Category
 class IncidentCategoryView(View):
     def get(self, request):
-        lng = request.headers.get('Accept-Language', 'en-CA')
+        lng = request.lng
         try:
             categories = IncidentCategory.objects.all()
-            # Change this to for loop readable
-            category_list = [{'id': category._id,
-                              'name': category.name, }
-                             for category in categories]
-            return JsonResponse({'categories': category_list})
+            category_list = []
+            for category in categories:
+                category_list.append({
+                    'id': category._id,
+                    'name': category.name,
+                })
+            return JsonResponse({
+                'message': MESSAGES[lng].get('SUCCESS_MESSAGE'),
+                'data': category_list,
+                'error': False,
+                'status': HTTPStatus.OK
+            }, status=HTTPStatus.OK)
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            return JsonResponse({
+                'message': MESSAGES[lng]
+                .get('ERROR_MESSAGE_INVALID_TOKEN').format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.BAD_REQUEST
+            }, status=HTTPStatus.BAD_REQUEST)
 
 
-# API Logic Incident
+# API Logic Incident reports
 class IncidentView(View):
-
-    # API Logic for retrieving single incident
+    # Code logic to retrieve incidents
     def get(self, request):
-        # return JsonResponse({'message': 'Hello single report'})
+        lng = request.lng
         try:
             incident_id = request.params.get('id')
+            if not incident_id:
+                raise ValueError("Incident ID is required")
+
             incident = get_object_or_404(Incident, _id=incident_id)
             single_incident = {
                 'id': str(incident._id),
@@ -59,84 +76,132 @@ class IncidentView(View):
                 'updated_at': incident.updated_at.isoformat(),
                 'voters': incident.voters
             }
-            return JsonResponse(single_incident, status=200)
+            return JsonResponse({
+                'message': MESSAGES[lng].get('SUCCESS_MESSAGE'),
+                'data': single_incident,
+                'error': False,
+                'status': HTTPStatus.OK
+            }, status=HTTPStatus.OK)
+        except ValueError as ve:
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_INVALID_TOKEN').format(str(ve)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.BAD_REQUEST
+            }, status=HTTPStatus.BAD_REQUEST)
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_INVALID_TOKEN').format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.INTERNAL_SERVER_ERROR
+            }, status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
-    # ***********  API logic for reporting an incident  *************************
+    # Code logic to report an incident
+
     def post(self, request):
-        lng = request.headers.get('Accept-Language', 'en-CA')
-    # try:
-        # Extract JSON data from POST
+        lng = request.lng
+        try:
+            data = json.loads(request.POST['json_data'])
 
-        print("This is json data")
-        print(request.POST['json_data'])
-        data = json.loads(request.POST['json_data'])
+            uploaded_images = request.FILES.getlist('images')
 
-        uploaded_images = request.FILES.getlist('images')
+            user_info = request.user_info
+            incident_category_id = data.get('incident_category_id')
+            subject = data.get('subject')
+            description = data.get('description')
+            coordinate = data.get('coordinate')
+            status = data.get('status')
+            is_active = data.get('is_active', True)
+            reported_by = data.get('reported_by')
+            is_internal_for_org = data.get('is_internal_for_org', False)
 
-        print(data)
+            user_id = get_user_model().objects.get(_id=user_info.get('_id'))
+            incident_category = IncidentCategory.objects.get(
+                _id=incident_category_id)
 
-        user_info = request.user_info
-        incident_category_id = data.get('incident_category_id')
-        subject = data.get('subject')
-        description = data.get('description')
-        coordinate = data.get('coordinate')
-        status = data.get('status')
-        is_active = data.get('is_active', True)
-        reported_by = data.get('reported_by')
-        is_internal_for_org = data.get('is_internal_for_org', False)
-
-        user_id = get_user_model().objects.get(_id=user_info.get('_id'))
-        incident_category = IncidentCategory.objects.get(
-            _id=incident_category_id)
-
-        print(user_id)
-
-        incident = Incident.objects.create(
-            user_id=user_id,
-            incident_category_id=incident_category,
-            subject=subject,
-            description=description,
-            coordinate=coordinate,
-            status=status,
-            is_active=is_active,
-            reported_by=reported_by,
-            is_internal_for_org=is_internal_for_org
-        )
-
-        print(incident)
-
-        # uploaded_images = request.FILES.getlist('images')
-
-        for image in uploaded_images:
-            # Upload image to S3 and get the URL
-            file_url = upload_file_to_s3(image)
-
-            # Create IncidentImage object and link to Incident
-            incident_image = IncidentImage.objects.create(
-                image=file_url
+            incident = Incident.objects.create(
+                user_id=user_id,
+                incident_category_id=incident_category,
+                subject=subject,
+                description=description,
+                coordinate=coordinate,
+                status=status,
+                is_active=is_active,
+                reported_by=reported_by,
+                is_internal_for_org=is_internal_for_org
             )
-            incident.images.add(incident_image)
 
-        # Return success response
-        return JsonResponse({'message': 'Incident reported successfully'}, status=201)
+            for image in uploaded_images:
+                # Upload image to S3 and get the URL
+                file_url = upload_file_to_s3(image)
 
-        # except Exception as e:
-        #     return JsonResponse({'error': str(e)}, status=500)
+                # Create IncidentImage object and link to Incident
+                incident_image = IncidentImage.objects.create(
+                    image=file_url
+                )
+                incident.images.add(incident_image)
 
-    # API Logic for Voting an Incident
+            # Return success response
+            return JsonResponse({
+                'message': MESSAGES[lng].get('SUCCESS_MESSAGE'),
+                'data': {'incident_id': str(incident._id)},
+                'error': False,
+                'status': HTTPStatus.CREATED
+            }, status=HTTPStatus.CREATED)
+        except json.JSONDecodeError as jde:
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_INVALID_DATA').format(str(jde)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.BAD_REQUEST
+            }, status=HTTPStatus.BAD_REQUEST)
+        except get_user_model().DoesNotExist as dne:
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').format('User'),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.NOT_FOUND
+            }, status=HTTPStatus.NOT_FOUND)
+        except IncidentCategory.DoesNotExist as dne:
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').format('Incident Category'),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.NOT_FOUND
+            }, status=HTTPStatus.NOT_FOUND)
+        except Exception as e:
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_INVALID_TOKEN').format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.INTERNAL_SERVER_ERROR
+            }, status=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    # Code Logic for Voting an Incident
 
     def put(self, request):
+        lng = request.lng
         try:
             incident_id = request.params.get('id')
+            if not incident_id:
+                return JsonResponse({
+                    'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').format('Incident ID is required'),
+                    'data': None,
+                    'error': True,
+                    'status': HTTPStatus.BAD_REQUEST
+                }, status=HTTPStatus.BAD_REQUEST)
+
             user_info = request.user_info
             incident = get_object_or_404(Incident, _id=incident_id)
 
             if incident.voters.filter(id=user_info._id).exists():
                 return JsonResponse({
-                    'already_voted': True
-                }, status=200)
+                    'message': MESSAGES[lng].get('ALREADY_VOTED_MESSAGE'),
+                    'already_voted': True,
+                    'error': False,
+                    'status': HTTPStatus.OK
+                }, status=HTTPStatus.OK)
 
             # Add the user to the voters of the incident
             incident.voters.add(user_info._id)
@@ -146,16 +211,24 @@ class IncidentView(View):
             incident.save()
 
             return JsonResponse({
-                'message': 'Your vote has been recorded.',
-                'voted': True
-            }, status=200)
+                'message': MESSAGES[lng].get('SUCCESS_VOTE_MESSAGE'),
+                'voted': True,
+                'error': False,
+                'status': HTTPStatus.OK
+            }, status=HTTPStatus.OK)
 
         except Incident.DoesNotExist:
             return JsonResponse({
-                'error': f'Incident with ID {incident_id} does not exist.'
-            }, status=404)
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').format(f'Incident with ID {incident_id} does not exist'),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.NOT_FOUND
+            }, status=HTTPStatus.NOT_FOUND)
 
         except Exception as e:
             return JsonResponse({
-                'error': f'An error occurred while processing your vote: {str(e)}'
-            }, status=500)
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_INVALID_TOKEN').format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.INTERNAL_SERVER_ERROR
+            }, status=HTTPStatus.INTERNAL_SERVER_ERROR)
