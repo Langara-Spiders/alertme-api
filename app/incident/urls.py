21d1fb0ad9eec558af5a2058ee48bae40abc0144
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-06-21 18:16:20 -0700

    feat: api linting checks completed

--- app/incident/urls.py
+++ app/incident/urls.py
@@ -1,15 +1,20 @@
 """
 URL mappings for the incident API
 """
+
 from django.urls import path
 from .views import (
     IncidentView,
     IncidentCategoryView,
 )
 
-app_name = 'incident'
+app_name = "incident"
 
 urlpatterns = [
-    path('category', IncidentCategoryView.as_view(), name='incident-categories'),
-    path('', IncidentView.as_view(), name='incidents'),
+    path(
+        "category",
+        IncidentCategoryView.as_view(),
+        name="incident-categories",
+    ),
+    path("", IncidentView.as_view(), name="incidents"),
 ]
