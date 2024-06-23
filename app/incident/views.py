942b7b2a11c508a2a24c7f5fd8a41883822ce452
Nandan42 <46378413+Nandan42@users.noreply.github.com>
2024-06-22 17:53:16 -0700

    fix: linting checks one more time

--- app/incident/views.py
+++ app/incident/views.py
@@ -1,19 +1,20 @@
 # Replace with your User model import
-
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model
+)
 from django.http import JsonResponse
 from django.views import View
 import json
 from core.utils import upload_file_to_s3
-from core.models import IncidentImage, Incident, IncidentCategory
+from core.models import IncidentImage, Incident
+from core.models import IncidentCategory
 from django.shortcuts import get_object_or_404
 from http import HTTPStatus
 from .messages import MESSAGES
 from core.utils import haversine
 
+
 # API Logic for Incident Category
-
-
 class IncidentCategoryView(View):
     def get(self, request):
         lng = request.lng
@@ -21,43 +22,30 @@ class IncidentCategoryView(View):
             categories = IncidentCategory.objects.all()
             category_list = []
             for category in categories:
-                category_list.append(
-                    {
-                        "id": category._id,
-                        "name": category.name,
-                    }
-                )
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng].get(
-                        "SUCCESS_MESSAGE_FOR_INCIDENT_CATEGORY"
-                    ),
-                    "data": category_list,
-                    "error": False,
-                    "status": HTTPStatus.OK,
-                },
-                status=HTTPStatus.OK,
-            )
+                category_list.append({
+                    'id': category._id,
+                    'name': category.name,
+                })
+            return JsonResponse({
+                'message': MESSAGES[lng].
+                get('SUCCESS_MESSAGE_FOR_INCIDENT_CATEGORY'),
+                'data': category_list,
+                'error': False,
+                'status': HTTPStatus.OK
+            }, status=HTTPStatus.OK)
         except Exception as e:
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng]
-                    .get("ERROR_MESSAGE_FOR_INCIDENT_CATEGORY")
-                    .format(str(e)),
-                    "data": None,
-                    "error": True,
-                    "status": HTTPStatus.BAD_REQUEST,
-                },
-                status=HTTPStatus.BAD_REQUEST,
-            )
+            return JsonResponse({
+                'message': MESSAGES[lng]
+                .get('ERROR_MESSAGE_FOR_INCIDENT_CATEGORY').format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.BAD_REQUEST
+            }, status=HTTPStatus.BAD_REQUEST)
 
 
 # API Logic Incident reports
-
-
 class IncidentView(View):
     # Code logic to retrieve incidents
-
     def get(self, request):
         lng = request.lng
         try:
@@ -187,30 +175,26 @@ class IncidentView(View):
             )
 
     # Code logic to report an incident
-
     def post(self, request):
         lng = request.lng
         try:
-            data = json.loads(request.POST["json_data"])
+            data = json.loads(request.POST['json_data'])
 
-            uploaded_images = request.FILES.getlist("images")
+            uploaded_images = request.FILES.getlist('images')
 
             user_info = request.user_info
-            incident_category_id = data.get("incident_category_id")
-            subject = data.get("subject")
-            description = data.get("description")
-            coordinate = data.get("coordinate")
-            status = data.get("status")
-            is_active = data.get("is_active", True)
-            reported_by = data.get("reported_by")
-            is_internal_for_org = data.get("is_internal_for_org", False)
+            incident_category_id = data.get('incident_category_id')
+            subject = data.get('subject')
+            description = data.get('description')
+            coordinate = data.get('coordinate')
+            status = data.get('status')
+            is_active = data.get('is_active', True)
+            reported_by = data.get('reported_by')
+            is_internal_for_org = data.get('is_internal_for_org', False)
 
-            user_id = get_user_model().objects.get(
-                _id=user_info.get("_id")
-            )
+            user_id = get_user_model().objects.get(_id=user_info.get('_id'))
             incident_category = IncidentCategory.objects.get(
-                _id=incident_category_id
-            )
+                _id=incident_category_id)
 
             incident = Incident.objects.create(
                 user_id=user_id,
@@ -221,149 +205,115 @@ class IncidentView(View):
                 status=status,
                 is_active=is_active,
                 reported_by=reported_by,
-                is_internal_for_org=is_internal_for_org,
+                is_internal_for_org=is_internal_for_org
             )
 
             for image in uploaded_images:
                 # Upload image to S3 and get the URL
-
                 file_url = upload_file_to_s3(image)
 
                 # Create IncidentImage object and link to Incident
-
                 incident_image = IncidentImage.objects.create(
                     image=file_url
                 )
                 incident.images.add(incident_image)
-            # Return success response
 
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng].get("SUCCESS_REPORT_MESSAGE"),
-                    "data": {"incident_id": str(incident._id)},
-                    "error": False,
-                    "status": HTTPStatus.CREATED,
-                },
-                status=HTTPStatus.CREATED,
-            )
+            # Return success response
+            return JsonResponse({
+                'message': MESSAGES[lng].get('SUCCESS_REPORT_MESSAGE'),
+                'data': {'incident_id': str(incident._id)},
+                'error': False,
+                'status': HTTPStatus.CREATED
+            }, status=HTTPStatus.CREATED)
+
         except Exception as e:
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng]
-                    .get("ERROR_REPORT_MESSAGE")
-                    .format(str(e)),
-                    "data": None,
-                    "error": True,
-                    "status": HTTPStatus.INTERNAL_SERVER_ERROR,
-                },
-                status=HTTPStatus.INTERNAL_SERVER_ERROR,
-            )
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_REPORT_MESSAGE').
+                format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.INTERNAL_SERVER_ERROR
+            }, status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # Code Logic for Voting an Incident
-
     def put(self, request):
         lng = request.lng
         try:
-            incident_id = request.GET.get("id")
+            incident_id = request.GET.get('id')
             if not incident_id:
-                return JsonResponse(
-                    {
-                        "message": MESSAGES[lng]
-                        .get("ERROR_MESSAGE_NOT_FOUND")
-                        .format("Incident ID is required"),
-                        "data": None,
-                        "error": True,
-                        "status": HTTPStatus.BAD_REQUEST,
-                    },
-                    status=HTTPStatus.BAD_REQUEST,
-                )
+                return JsonResponse({
+                    'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').
+                    format('Incident ID is required'),
+                    'data': None,
+                    'error': True,
+                    'status': HTTPStatus.BAD_REQUEST
+                }, status=HTTPStatus.BAD_REQUEST)
+
             user_info = request.user_info
-            current_user_id = user_info.get("_id")
+            current_user_id = user_info.get('_id')
             incident = get_object_or_404(Incident, _id=incident_id)
 
             if incident.voters.filter(_id=current_user_id).exists():
-                return JsonResponse(
-                    {
-                        "message": MESSAGES[lng].get(
-                            "ALREADY_VOTED_MESSAGE"
-                        ),
-                        "already_voted": True,
-                        "error": False,
-                        "status": HTTPStatus.OK,
-                    },
-                    status=HTTPStatus.OK,
-                )
-            # Add the user to the voters of the incident
+                return JsonResponse({
+                    'message': MESSAGES[lng].get('ALREADY_VOTED_MESSAGE'),
+                    'already_voted': True,
+                    'error': False,
+                    'status': HTTPStatus.OK
+                }, status=HTTPStatus.OK)
 
+            # Add the user to the voters of the incident
             incident.voters.add(current_user_id)
 
             # Increment the upvote_count of the incident
-
             incident.upvote_count += 1
             incident.save()
 
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng].get("SUCCESS_VOTE_MESSAGE"),
-                    "voted": True,
-                    "error": False,
-                    "status": HTTPStatus.OK,
-                },
-                status=HTTPStatus.OK,
-            )
+            return JsonResponse({
+                'message': MESSAGES[lng].get('SUCCESS_VOTE_MESSAGE'),
+                'voted': True,
+                'error': False,
+                'status': HTTPStatus.OK
+            }, status=HTTPStatus.OK)
+
         except Exception as e:
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng]
-                    .get("ERROR_VOTE_MESSAGE")
-                    .format(str(e)),
-                    "data": None,
-                    "error": True,
-                    "status": HTTPStatus.INTERNAL_SERVER_ERROR,
-                },
-                status=HTTPStatus.INTERNAL_SERVER_ERROR,
-            )
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_VOTE_MESSAGE').
+                format(str(e)),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.INTERNAL_SERVER_ERROR
+            }, status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
     # Code Logic for Deleting an Incident
-
     def delete(self, request):
         lng = request.lng
-        incident_id = request.GET.get("id")
+        incident_id = request.GET.get('id')
         if not incident_id:
-            return JsonResponse(
-                {
-                    "message": MESSAGES[lng]
-                    .get("ERROR_MESSAGE_NOT_FOUND")
-                    .format("Incident ID is required"),
-                    "data": None,
-                    "error": True,
-                    "status": HTTPStatus.BAD_REQUEST,
-                },
-                status=HTTPStatus.BAD_REQUEST,
-            )
+            return JsonResponse({
+                'message': MESSAGES[lng].get('ERROR_MESSAGE_NOT_FOUND').
+                format('Incident ID is required'),
+                'data': None,
+                'error': True,
+                'status': HTTPStatus.BAD_REQUEST
+            }, status=HTTPStatus.BAD_REQUEST)
+
         incident = get_object_or_404(Incident, _id=incident_id)
 
         # Delete related images
-
         for image in incident.images.all():
             incident.images.remove(image)
             image.delete()
+
         incident.voters.clear()
 
         # Delete the incident itself
-
         incident.delete()
 
-        return JsonResponse(
-            {
-                "message": MESSAGES[lng].get(
-                    "SUCCESS_MESSAGE_INCIDENT_DELETED"
-                ),
-                "error": False,
-                "status": HTTPStatus.OK,
-            },
-            status=HTTPStatus.OK,
-        )
+        return JsonResponse({
+            'message': MESSAGES[lng].get('SUCCESS_MESSAGE_INCIDENT_DELETED'),
+            'error': False,
+            'status': HTTPStatus.OK
+        }, status=HTTPStatus.OK)
 
     def get_nearby(self, request):
         lng = request.lng
