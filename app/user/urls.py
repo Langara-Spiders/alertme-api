662489f497db1949f9f639b69b514e3e3fef98d6
BryanCaldeira <bryancaldeira22@gmail.com>
2024-06-25 16:53:57 -0700

    chore: 171 | user endpoints add picture and restructuring

--- app/user/urls.py
+++ app/user/urls.py
@@ -13,5 +13,5 @@ app_name = 'user'
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('signup', SignupView.as_view(), name='signup'),
-    path('profile', ProfileView.as_view(), name='profile')
+    path('profile', ProfileView.as_view(), name='profile'),
 ]
