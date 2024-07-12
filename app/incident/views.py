5bc13e64b7164046d9088b3a473f98688e88880b
BryanCaldeira <bryancaldeira22@gmail.com>
2024-07-12 09:55:47 -0700

    fix: patch for images

--- app/incident/views.py
+++ app/incident/views.py
@@ -352,6 +352,7 @@ class IncidentReportView(View):
             # Set the images to incident if present
             if images:
                 incident.images.set(images)
+                incident.save()
 
             # Create notification for nearby users
             threading.Thread(
