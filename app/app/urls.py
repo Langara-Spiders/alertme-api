4322f4dacfa8100fb42b491ef0e6ab2b2a04aa25
BryanCaldeira <bryancaldeira22@gmail.com>
2024-08-07 21:14:41 -0700

    fix: re_path added

--- app/app/urls.py
+++ app/app/urls.py
@@ -7,5 +7,5 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/users/', include('user.urls')),
-    path(r'^v1/incidents/?', include('incident.urls')),
+    re_path(r'^v1/incidents/?', include('incident.urls')),
 ]
